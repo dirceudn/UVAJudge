80ef06f40e61c9a14ee86aa559569ba0a082421b
dirceu de medeiros teixeira <dirceu@MacBook-Pro-de-dirceu.local>
2019-08-18 12:13:00 +0200

    setup the projet c++

---
+++ cmake-build-debug/CMakeFiles/UvaJudge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UvaJudge.dir/main.cpp.o"
+  "UvaJudge.pdb"
+  "UvaJudge"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UvaJudge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
